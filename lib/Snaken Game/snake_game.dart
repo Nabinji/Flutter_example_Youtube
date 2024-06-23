916ba59f00edecd442d9d5c91353d0c9297dec61
Nabinji <nabinpaudyal2057@gmail.com>
2024-06-23 15:05:52 +0545

    snake game

--- lib/Snaken Game/snake_game.dart
+++ lib/Snaken Game/snake_game.dart
@@ -2,6 +2,7 @@
 
 import 'dart:async';
 import 'dart:math';
+
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 
@@ -15,15 +16,14 @@ class SnakeGamePage extends StatefulWidget {
 enum Direction { up, down, left, right }
 
 class _SnakeGamePageState extends State<SnakeGamePage> {
-  int noOfRow = 38, noOfRcolumn = 21;
+  int noOfRow = 38, noOfColumn = 21;
   List<int> borderList = [];
   List<int> snakePosition = [];
-  int snakeHead = 0;
+  int snakeHeade = 0;
   int score = 0;
-  late Direction direction;
   late int foodPosition;
   late FocusNode focusNode;
-
+  late Direction direction;
   @override
   void initState() {
     super.initState();
@@ -31,112 +31,108 @@ class _SnakeGamePageState extends State<SnakeGamePage> {
     startGame();
   }
 
-  @override
-  void dispose() {
+  void despose() {
     focusNode.dispose();
-    super.dispose();
+    super.initState();
   }
 
   void startGame() {
     setState(() {
-      score = 0; // Reset score in every new game
+      score = 0; // reset score in every new game
       makeBorder();
       generateFood();
-      direction = Direction.right; // Set the initial direction of the snake
-      snakePosition = [50, 49, 48]; // Set the initial position of the snake
-      snakeHead = snakePosition.first;
+      direction = Direction.right; // set the initial direction of the snake
+      snakePosition = [50, 49, 48]; // set he initial position of the snake
+      snakeHeade = snakePosition.first;
     });
-// Set up a periodic timer to update the snake's position
-    Timer.periodic(const Duration(milliseconds: 300), (timer) {
+    // set up a periodic time to update the snake's posttion
+    Timer.periodic(Duration(milliseconds: 300), (timer) {
       updateSnake();
       if (checkCollision()) {
         timer.cancel();
-        showGameOverDialog();
+        showDialogBox();
       }
     });
   }
 
-  void showGameOverDialog() {
+  void showDialogBox() {
     showDialog(
-      context: context,
-      barrierDismissible: false,
-      builder: (context) {
-        // dialog box for restart the game and show final score
-        return AlertDialog(
-          title: const Text("Game Over"),
-          content: Text(
-            "Your Final Score is: $score",
-            style: const TextStyle(
-              fontWeight: FontWeight.bold,
-              color: Colors.green,
-              fontSize: 18,
+        context: context,
+        builder: (context) {
+          return AlertDialog(
+            title: const Text("Game Over"),
+            content: Text(
+              "Your Final Score is: $score",
+              style: const TextStyle(
+                fontWeight: FontWeight.bold,
+                fontSize: 18,
+                color: Colors.green,
+              ),
             ),
-          ),
-          actions: [
-            TextButton(
-              onPressed: () {
-                Navigator.of(context).pop();
-                startGame();
-              },
-              child: const Text("Restart"),
-            ),
-          ],
-        );
-      },
-    );
+            actions: [
+              TextButton(
+                  onPressed: () {
+                    Navigator.of(context).pop();
+                    startGame();
+                  },
+                  child: const Text(
+                    "Restart",
+                  ))
+            ],
+          );
+        });
   }
 
-  // Check if the snake collides with the border or itself
+  // check if the snake collides with the border or itself
   bool checkCollision() {
-    if (borderList.contains(snakeHead)) return true;
-    if (snakePosition.sublist(1).contains(snakeHead)) return true;
+    if (borderList.contains(snakeHeade)) return true;
+    if (snakePosition.sublist(1).contains(snakeHeade)) return true;
     return false;
   }
 
   void generateFood() {
-    // Generate a random position for the food
-    foodPosition = Random().nextInt(noOfRow * noOfRcolumn);
-    // Ensure the food is not placed on the border or the snake
+    // fenerate a random postion of the food
+    foodPosition = Random().nextInt(noOfRow * noOfColumn);
+    // ensure the food is not placed on the border or the snake itself
     if (borderList.contains(foodPosition) ||
         snakePosition.contains(foodPosition)) {
       generateFood();
     }
   }
 
-  // Update the snake's head position based on the current direction
+  // UPDATE THE SNAKE'S HEAD POSITION BASED ON THE CURRENT DIRECTION
   void updateSnake() {
     setState(() {
       switch (direction) {
         case Direction.up:
-          snakePosition.insert(0, snakeHead - noOfRcolumn);
+          snakePosition.insert(0, snakeHeade - noOfColumn);
           break;
         case Direction.down:
-          snakePosition.insert(0, snakeHead + noOfRcolumn);
+          snakePosition.insert(0, snakeHeade + noOfColumn);
           break;
         case Direction.right:
-          snakePosition.insert(0, snakeHead + 1);
+          snakePosition.insert(0, snakeHeade + 1);
           break;
         case Direction.left:
-          snakePosition.insert(0, snakeHead - 1);
+          snakePosition.insert(0, snakeHeade - 1);
           break;
       }
     });
-// Check if the snake has eaten the food
-    if (snakeHead == foodPosition) {
-      score++; // Increase the score
-      generateFood(); // Generate a new food position
+    // check if the snake has eaten the food
+    if (snakeHeade == foodPosition) {
+      score++; // increase the score
+      generateFood(); // generate a new food position
     } else {
       snakePosition.removeLast();
     }
-
-    snakeHead = snakePosition.first;
+    snakeHeade = snakePosition.first;
   }
 
-  // Handle keyboard input to change the snake's direction
-  void handleKey(RawKeyEvent event) {
+// it handle keyboard input to change the snake's direction
+  void handledKey(RawKeyEvent event) {
     if (event is RawKeyDownEvent) {
       switch (event.logicalKey) {
-        case LogicalKeyboardKey.arrowUp: // Arrow Up
+        case LogicalKeyboardKey.arrowUp: // Arrow up
           if (direction != Direction.down) direction = Direction.up;
           break;
         case LogicalKeyboardKey.arrowDown: // Arrow Down
@@ -159,19 +155,21 @@ class _SnakeGamePageState extends State<SnakeGamePage> {
       body: SafeArea(
         child: RawKeyboardListener(
           focusNode: focusNode,
-          onKey: handleKey,
+          onKey: handledKey,
           autofocus: true,
           child: Column(
             children: [
               Text(
-                "Score: $score",
-                style: const TextStyle(
+                "Score:$score",
+                style: TextStyle(
                   fontSize: 25,
                   fontWeight: FontWeight.bold,
                   color: Colors.green,
                 ),
               ),
-              Expanded(child: snakeGround()),
+              Expanded(
+                child: groundForSnake(),
+              ),
             ],
           ),
         ),
@@ -179,31 +177,32 @@ class _SnakeGamePageState extends State<SnakeGamePage> {
     );
   }
 
-  Widget snakeGround() {
+  Widget groundForSnake() {
     return GridView.builder(
+      itemCount: noOfRow * noOfColumn,
       gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-          crossAxisCount: noOfRcolumn),
+        crossAxisCount: noOfColumn,
+      ),
       itemBuilder: (context, index) {
         return Container(
-          color: fillBoxColor(index),
+          color: boxFillColor(index),
           margin: const EdgeInsets.only(right: 1, bottom: 1),
         );
       },
-      itemCount: noOfRow * noOfRcolumn,
     );
   }
 
-  Color fillBoxColor(int index) {
+// now we continue to code this error is solved doring next step
+  Color boxFillColor(int index) {
     if (borderList.contains(index)) {
-      //out boder color
+      // outer border color
       return Colors.blue;
     } else {
       if (snakePosition.contains(index)) {
-        if (snakeHead == index) {
+        if (snakeHeade == index) {
           // snake head color
           return Colors.green;
         } else {
-          // snake body color
           return Colors.black45;
         }
       } else {
@@ -217,23 +216,24 @@ class _SnakeGamePageState extends State<SnakeGamePage> {
     return Colors.white60;
   }
 
+  // outer border of ground
   void makeBorder() {
-    borderList.clear(); // Ensure borderList is cleared before adding borders
-    // Add the top border cells
-    for (int i = 0; i < noOfRcolumn; i++) {
+    borderList.clear(); // ensure borderList is cleared before adding bordrs
+    // add the top border cells
+    for (int i = 0; i < noOfColumn; i++) {
       borderList.add(i);
     }
-    // Add the left border cells
-    for (int i = 0; i < noOfRow * noOfRcolumn; i += noOfRcolumn) {
+    // add the left border cells
+    for (int i = 0; i < noOfRow * noOfColumn; i += noOfColumn) {
       borderList.add(i);
     }
-    // Add the right border cells
-    for (int i = noOfRcolumn - 1; i < noOfRow * noOfRcolumn; i += noOfRcolumn) {
+    // add the right border cells
+    for (int i = noOfColumn - 1; i < noOfRow * noOfColumn; i += noOfColumn) {
       borderList.add(i);
     }
-    // Add the bottom border cells
-    for (int i = (noOfRow * noOfRcolumn) - noOfRcolumn;
-        i < noOfRow * noOfRcolumn;
+    // add the bottom border cells
+    for (int i = (noOfRow * noOfColumn) - noOfColumn;
+        i < noOfRow * noOfColumn;
         i++) {
       borderList.add(i);
     }
--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_example/Snaken%20Game/snake_game.dart';
 
-
-
-
 void main() {
   runApp(const MyApp());
 }
+
 // This widget is the root of your application.
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
