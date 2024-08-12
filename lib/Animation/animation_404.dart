02d9d4050d8eb0f455881e9fa6350500fd65de5f
Nabinji <nabinpaudyal2057@gmail.com>
2024-08-12 07:28:31 +0545

    update code

--- lib/Animation/animation_404.dart
+++ lib/Animation/animation_404.dart
@@ -4,112 +4,103 @@ class Flutter404Animation extends StatefulWidget {
   const Flutter404Animation({super.key});
 
   @override
-  _Flutter404AnimationState createState() => _Flutter404AnimationState();
+  State<Flutter404Animation> createState() => _Flutter404AnimationState();
 }
 
 class _Flutter404AnimationState extends State<Flutter404Animation>
     with SingleTickerProviderStateMixin {
-  // AnimationController will control the animation.
-  late AnimationController _controller;
+  // animationcontroller will control tha animation.
+  late AnimationController _animationController;
 
   @override
   void initState() {
-    // Initialize the AnimationController with a duration of 2 seconds for both
-    // forward and reverse animations.
-    _controller = AnimationController(
+    // Initialize the animationcontroller wit a duration 2 secnds for both forward and reverse animation
+    _animationController = AnimationController(
+      vsync: this,
       duration: const Duration(seconds: 2),
       reverseDuration: const Duration(seconds: 2),
-      vsync: this,
     )
-      // Add a listener to check the status of the animation.
-      // If the animation is completed, it reverses back.
-      // If it is dismissed (reached the beginning), it forwards again.
+      // add a listener to check the status of the animation.
+      // if the animation is completed, it reverse back.
+      // if it si dismissed (reached the beginning), it forwards again.
+
       ..addStatusListener((status) {
         if (status == AnimationStatus.completed) {
-          _controller.reverse();
+          _animationController.reverse();
         } else if (status == AnimationStatus.dismissed) {
-          _controller.forward();
+          _animationController.forward();
         }
       });
-
-    // Start the animation in the forward direction.
-    _controller.forward();
+    // start the naimation in the forward direction.
+    _animationController.forward();
     super.initState();
   }
 
-  @override
-  void dispose() {
-    _controller.dispose();
+  void despose() {
+    _animationController.dispose();
     super.dispose();
   }
 
   @override
   Widget build(BuildContext context) {
     double height = MediaQuery.of(context).size.height;
-
     return Scaffold(
       backgroundColor: Theme.of(context).primaryColor,
       body: Stack(
         children: [
-          // The first positioned widget displays an animated image that rotates back and forth.
+          // the first positioned widget displays an animated image that totates back and forth.
           Positioned(
             bottom: height / 4.4,
             right: height / 20,
             child: AnimatedBuilder(
-                // The AnimatedBuilder listens to changes in the AnimationController and rebuilds
-                // the widget tree with each tick of the animation.
-                animation: _controller,
-                child: Image.asset(
-                  'image/light.png',
-                  height: height * 1.3,
-                ),
-                builder: (BuildContext context, widget) {
-                  // The Transform widget is used to rotate the image around its bottom-center point.
-                  return Transform.rotate(
-                    angle: -_controller
-                        .value, // Rotate the image according to the animation value.
-                    alignment: Alignment.bottomCenter,
-                    child: widget,
-                  );
-                }),
+              animation: _animationController,
+              child: Image.asset(
+                "image/light.png",
+                height: height * 1.3,
+              ),
+              builder: (context, widget) {
+                // the transform widget is used to rotate the image around it's bottom-center point.
+                return Transform.rotate(
+                  angle:- _animationController
+                      .value, // rotate the aimage according to tha animation value
+                  alignment: Alignment.bottomCenter,
+                  child: widget,
+                );
+              },
+            ),
           ),
 
           Positioned(
             bottom: 0,
             right: height / 2.7,
             child: Image.asset(
-              'image/source.png',
+              "image/source.png",
               width: height / 4.6,
             ),
           ),
-
           Center(
             child: Column(
               children: [
                 Text(
-                  '404',
+                  "404",
                   style: TextStyle(
-                    fontSize: height /
-                        5, // Set the font size relative to the screen height.
-                    fontWeight: FontWeight.bold, // Make the text bold.
-                    color: Theme.of(context)
-                        .primaryColor, // Use the theme's primary color.
+                    fontSize: height / 5,
+                    fontWeight: FontWeight.bold,
+                    color: Theme.of(context).primaryColor,
                   ),
                 ),
                 Text(
-                  'Page Not Found',
+                  "Page Not Found",
+                  textAlign: TextAlign.center,
                   style: TextStyle(
-                    fontSize: height *
-                        0.05, // Set the font size relative to the screen height.
-                    fontWeight: FontWeight.bold, // Make the text bold.
-                    color: Theme.of(context)
-                        .primaryColor, // Use the theme's primary color.
+                    fontSize: height * 0.05,
+                    fontWeight: FontWeight.bold,
+                    color: Theme.of(context).primaryColor,
                   ),
-                  textAlign: TextAlign.center, // Center the text horizontally.
                 )
               ],
             ),
-          ),
+          )
         ],
       ),
     );
